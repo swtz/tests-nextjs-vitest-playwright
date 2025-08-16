a324cdb34e5527ccf567215bd8f9e36e38216e2b
Leonardo Lüders <luderser@hotmail.com>
2025-08-16 15:04:41 -0300

    Finished class 286 — Organized test functions into nested blocks to improve readability.

--- src/core/todo/factories/make-new-todo.spec.ts
+++ src/core/todo/factories/make-new-todo.spec.ts
@@ -1,18 +1,22 @@
 import { makeNewTodo } from "./make-new-todo";
 
-it("should creates a new todo object", () => {
-  // AAA → Arrange, Act, Assert
+describe("makeNewTodo", () => {
+  describe("create", () => {
+    it("should creates a new todo object", () => {
+      // AAA → Arrange, Act, Assert
 
-  // Arrange → criar o que precisa antes de executar o teste
-  const mockNewTodo = {
-    id: "any-id",
-    description: "hello, world!",
-    createdAt: "any-date",
-  };
+      // Arrange → criar o que precisa antes de executar o teste
+      const mockNewTodo = {
+        id: expect.any(String),
+        description: "hello, world!",
+        createdAt: expect.any(String),
+      };
 
-  // Act → usar a função ou recurso que se quer checar o comportamento
-  const newTodo = makeNewTodo("hello, world!");
+      // Act → usar a função ou recurso que se quer checar o comportamento
+      const newTodo = makeNewTodo("hello, world!");
 
-  // Assert → verificar se o comportamento ocorre como esperado (ou como não esperado)
-  expect(newTodo.description).toBe(mockNewTodo.description);
+      // Assert → verificar se o comportamento ocorre como esperado (ou como não esperado)
+      expect(newTodo).toStrictEqual(mockNewTodo);
+    });
+  });
 });
